77f8e282b8d48c8e89a1db45f2a6d8033a47bd5d
Victor Accete <victor.accete@gmail.com>
2020-10-17 02:15:40 -0300

    fixing multiline comment

--- df_generator.py
+++ df_generator.py
@@ -3,8 +3,7 @@ Author: @victoraccete
 Please note that this script was made in 2020 to get data from 2007 to 2019.
 It might not be maintained to keep up to date.
 The goal of this code is to generate a .csv with data from 2007 to 2019 to then
-make a EDA on this data.
-'''
+make a EDA on this data.'''
 import pandas as pd
 
 def get_tables():
@@ -16,7 +15,7 @@ def df_dict(tables: list) -> dict:
     '''
     Returns a dictionary where the key is the year and the value is
     the corresponding dataframe.
-    ''''
+    '''
     df_dict = {}
     tab_it = iter(tables)
     for year in range(2019, 2006, -1):
@@ -24,4 +23,11 @@ def df_dict(tables: list) -> dict:
     return df_dict
 
 
-print(df_dict(get_tables()).keys())
+
+############## Main block ##############
+def main():
+    print(df_dict(get_tables()).keys())
+
+if __name__ == "__main__":
+    main()
+########## end of main block ##########
