5c3c948999e1e5bacbf0b621a7483faa6cb1a375
Victor Accete <victor.accete@gmail.com>
2020-10-17 01:50:13 -0300

    adding disclaimer

--- df_generator.py
+++ df_generator.py
@@ -1,3 +1,10 @@
+'''
+Author: @victoraccete
+Please note that this script was made in 2020 to get data from 2007 to 2019.
+It might not be maintained to keep up to date.
+The goal of this code is to generate a .csv with data from 2007 to 2019 to then
+make a EDA on this data.
+'''
 import pandas as pd
 
 def get_tables():
