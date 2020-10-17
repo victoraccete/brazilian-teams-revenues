9ae8a42df71294f412f74443cec0444c1fffb049
Victor Accete <victor.accete@gmail.com>
2020-10-17 01:47:11 -0300

    putting inside function

--- df_generator.py
+++ df_generator.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
-URL = 'https://pt.wikipedia.org/wiki/Lista_de_faturamento_dos_clubes_de_futebol_brasileiro'
-tables_list = pd.read_html(URL)
+def get_tables():
+    URL = 'https://pt.wikipedia.org/wiki/Lista_de_faturamento_dos_clubes_de_futebol_brasileiro'
+    tables_list = pd.read_html(URL)
+    return tables_list
