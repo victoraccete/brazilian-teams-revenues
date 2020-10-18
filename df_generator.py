3ea0ef9f361e9b184fd7b926a3f05d156773de8e
Victor Accete <victor.accete@gmail.com>
2020-10-17 22:32:23 -0300

    making docstrings with double quotes for consistency

--- df_generator.py
+++ df_generator.py
@@ -1,10 +1,11 @@
 # This Python file uses the following encoding: utf-8
-'''
+"""
 Author: @victoraccete
 Please note that this script was made in 2020 to get data from 2007 to 2019.
 It might not be maintained to keep up to date.
 The goal of this code is to generate a .csv with data from 2007 to 2019 to then
-make a EDA on this data.'''
+make a EDA on this data.
+"""
 import pandas as pd
 
 GREAT_CLUBS = (
@@ -23,15 +24,16 @@ GREAT_CLUBS = (
         )
 
 def get_tables():
+    """From wikipedia"""
     URL = 'https://pt.wikipedia.org/wiki/Lista_de_faturamento_dos_clubes_de_futebol_brasileiro'
     tables_list = pd.read_html(URL)
     return tables_list
 
 def df_dict(tables: list) -> dict:
-    '''
+    """
     Returns a dictionary where the key is the year and the value is
     the corresponding dataframe.
-    '''
+    """
     df_dict = {}
     tab_it = iter(tables)
     for year in range(2019, 2006, -1):
@@ -44,9 +46,9 @@ def keep_greater_teams(tables: list) -> list:
         tables[index] = df[df.Clube.isin(GREAT_CLUBS)]
     return tables
 
-def include_years_to_df_list(tables: list) -> list:
+def include_years_to_dfs(tables: list) -> list:
      def include_year(df, year):
-         '''adds a new column to the given df with a given year'''
+         """adds a new column to the given df with a given year"""
          df['Ano'] = year
          return df
      # iterator used to help including years from 2019 to 2006 in descending order
@@ -58,7 +60,7 @@ def include_years_to_df_list(tables: list) -> list:
 ############## Main block ##############
 def main():
     tables = keep_greater_teams(get_tables())
-    tables = include_years_to_df_list(tables)
+    tables = include_years_to_dfs(tables)
     print(tables[0])
 
 if __name__ == "__main__":
