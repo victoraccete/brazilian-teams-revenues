4786a176a06f1f2990fdb55ec779f2f590fb6566
Victor Accete <victor.accete@gmail.com>
2020-10-22 01:55:34 -0300

    starting streamlit project

---
+++ streamlit_app/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+st.title("Análise da receita dos clubes brasileiros")
+st.sidebar.title("Opções de visualização")
