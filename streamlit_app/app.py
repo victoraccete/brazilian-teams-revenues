c36d52898004db180a39d4eddbc4dda71aa2d2ce
Victor Accete <victor.accete@gmail.com>
2020-10-31 18:09:32 -0300

    Adding descreption

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -3,5 +3,9 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 
-st.title("Análise da receita dos clubes brasileiros")
+st.title("Análise de faturamento dos clubes brasileiros")
+st.markdown("Apenas dados dos considerados '12 grandes'. \
+Dados extraídos da [Wikipedia](https://pt.wikipedia.org/wiki/Lista_de_faturamento_dos_clubes_de_futebol_brasileiro) \
+de 2007 a 2019.")
+st.markdown("Use a barra lateral para escolher opções de visualização.")
 st.sidebar.title("Opções de visualização")
